f6f201a3494dee3cc45d21220fc45265c07ecd94
Colm Talbot <colm.talbot@ligo.org>
2019-05-05 10:40:38 +1000

    make n_lorentzians sampleable

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from __future__ import division
 from scipy.interpolate import CubicSpline
 import numpy as np
 
+from bilby.core.prior import Prior
 from bilby.gw.detector import PowerSpectralDensity
 
 
@@ -69,7 +70,7 @@ class SplineLorentzianPSD(PowerSpectralDensity):
     @property
     def n_lorentzians(self):
         try:
-            return self.parameters['{}_n_lorentzians'.format(self.name)]
+            return int(self.parameters['{}_n_lorentzians'.format(self.name)])
         except KeyError:
             return len(
                 [key for key in self.parameters
@@ -78,12 +79,14 @@ class SplineLorentzianPSD(PowerSpectralDensity):
     @property
     def _lorentzian_frequencies_keys(self):
         try:
-            return self.__lorentzian_frequencies_keys
-        except AttributeError:
+            # return self.__lorentzian_frequencies_keys
+        # except AttributeError:
             self.__lorentzian_frequencies_keys = [
                 '{}_lorentzian_frequency_{}'.format(self.name, ii)
                 for ii in range(self.n_lorentzians)]
             return self.__lorentzian_frequencies_keys
+        except AttributeError:
+            return None
 
     @property
     def lorentzian_frequencies(self):
@@ -92,12 +95,14 @@ class SplineLorentzianPSD(PowerSpectralDensity):
     @property
     def _lorentzian_amplitudes_keys(self):
         try:
-            return self.__lorentzian_amplitudes_keys
-        except AttributeError:
+            # return self.__lorentzian_amplitudes_keys
+        # except AttributeError:
             self.__lorentzian_amplitudes_keys = [
                 '{}_lorentzian_amplitude_{}'.format(self.name, ii)
                 for ii in range(self.n_lorentzians)]
             return self.__lorentzian_amplitudes_keys
+        except AttributeError:
+            return None
 
     @property
     def lorentzian_amplitudes(self):
@@ -106,12 +111,14 @@ class SplineLorentzianPSD(PowerSpectralDensity):
     @property
     def _lorentzian_qualities_keys(self):
         try:
-            return self.__lorentzians_qualities_keys
-        except AttributeError:
+            # return self.__lorentzians_qualities_keys
+        # except AttributeError:
             self.__lorentzians_qualities_keys = [
                 '{}_lorentzian_quality_{}'.format(self.name, ii)
                 for ii in range(self.n_lorentzians)]
             return self.__lorentzians_qualities_keys
+        except AttributeError:
+            return None
 
     @property
     def lorentzian_qualities(self):
@@ -126,12 +133,14 @@ class SplineLorentzianPSD(PowerSpectralDensity):
 
     def lorentzian(self, frequency_array):
         lorentzian = 0 * frequency_array
+        # print(self.n_lorentzians)
         if self.n_lorentzians == 0:
             return lorentzian
         else:
             aas = self.lorentzian_amplitudes
             qqs = self.lorentzian_qualities
             ffs = self.lorentzian_frequencies
+            # print(qqs, aas, ffs)
             for ii in range(self.n_lorentzians):
                 lorentzian += self._single_lorentzian(
                     frequency_array, 10 ** aas[ii], 10 ** qqs[ii], ffs[ii])
@@ -141,3 +150,30 @@ class SplineLorentzianPSD(PowerSpectralDensity):
     def _single_lorentzian(frequency_array, amplitude, quality, location):
         return cauchy(f=frequency_array, f0=location, gamma=quality) * amplitude
 
+
+class Discrete(Prior):
+
+    def __init__(self, minimum, maximum, step_size, name=None,
+                 latex_label=None, boundary=None):
+        super(Discrete, self).__init__(
+            name=name, latex_label=latex_label, boundary=boundary)
+        self.minimum = minimum
+        self.maximum = maximum
+        self.step_size = step_size
+        if (maximum - minimum + 1) % step_size != 0:
+            raise ValueError(
+                'maximum - minimum must be an integer multiple of step size')
+
+    @property
+    def n_bins(self):
+        return (self.maximum - self.minimum + 1) / self.step_size
+
+    def prob(self, val):
+        prob = 1 / self.n_bins
+        return prob
+
+    def rescale(self, val):
+        val = np.atleast_1d(val)
+        val *= self.step_size * self.n_bins
+        val += self.minimum
+        return val.astype(int)
